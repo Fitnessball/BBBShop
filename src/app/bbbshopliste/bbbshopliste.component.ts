c1ebb3764b968300ba9e3f5906d1742a889cf38d
Fitnessball <78953708+Fitnessball@users.noreply.github.com>
2024-08-06 16:12:35 +0200

    Quality of life changes

--- src/app/bbbshopliste/bbbshopliste.component.ts
+++ src/app/bbbshopliste/bbbshopliste.component.ts
@@ -4,17 +4,36 @@ import { MatCardModule } from '@angular/material/card';
 import { MatChipsModule } from '@angular/material/chips';
 import { MatListModule } from '@angular/material/list';
 import { ArtikelService } from '../providers/artikel.service';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { FormsModule } from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+interface Food {
+  value: string;
+  viewValue: string;
+}
 
 @Component({
   selector: 'app-bbbshopliste',
   standalone: true,
-  imports: [MatTableModule, MatCardModule, MatChipsModule, MatListModule],
+  imports: [
+    MatTableModule,
+    MatCardModule,
+    MatChipsModule,
+    MatListModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    MatButtonModule
+    ],
   templateUrl: './bbbshopliste.component.html',
   styleUrls: ['./bbbshopliste.component.css']
 })
 export class BbbshoplisteComponent implements OnInit {
   title = 'WarenkorbSystem';
-
+  selectedValue = '';
   public artikel: any[] = [];
 
   displayedColumns: string[] = ['a_nr', 'artikel', 'kategorie', 'anzahl', 'gebinde'];
@@ -29,4 +48,13 @@ export class BbbshoplisteComponent implements OnInit {
       this.dataSource = this.artikel; // Update dataSource with the actual data
     });
   }
+  addNewRow() {}
+
+
+  foods: Food[] = [
+    {value: 'steak-0', viewValue: 'Steak'},
+    {value: 'pizza-1', viewValue: 'Pizza'},
+    {value: 'tacos-2', viewValue: 'Tacos'},
+  ];
+
 }
